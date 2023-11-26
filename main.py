447b02cea5a522d02ad6b3acd60af7a1ec3d05e3
wling <wlingzhenyu@163.com>
2023-11-26 17:17:55 +0800

    Add smooth movement to MouseTrackerLabel

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from PyQt5.QtWidgets import QApplication, QLabel
 from PyQt5.QtGui import QPixmap, QCursor
 from PyQt5.QtCore import Qt, QTimer, QPoint
 
+def lerp(a, b, n):
+    return (1 - n) * a + n * b
+
 
 class MouseTrackerLabel(QLabel):
 
@@ -10,9 +13,9 @@ class MouseTrackerLabel(QLabel):
         super().__init__(parent)
         # 创建一个无边框、置顶、半透明的窗口
         self.setWindowFlags(Qt.FramelessWindowHint |
-                            Qt.WindowStaysOnTopHint | Qt.WindowTransparentForInput)  # 设置窗口标志，使其无边框、置顶、不影响鼠标点击
-        self.setWindowOpacity(0.9)  # 设置窗口透明度
-        self.setAttribute(Qt.WA_TranslucentBackground)  # 设置窗口背景透明
+                            Qt.WindowStaysOnTopHint | Qt.WindowTransparentForInput)
+        self.setWindowOpacity(0.9)
+        self.setAttribute(Qt.WA_TranslucentBackground)
 
     def move_to_mouse(self):
         b = self.pos()
@@ -21,6 +24,20 @@ class MouseTrackerLabel(QLabel):
         for t in range(1, d + 1):
             self.move(b + c * t / d)
             QApplication.processEvents()
+
+    def smooth_move(self):
+        target_pos = QCursor.pos() - QPoint(self.width() // 3, self.height() // 3)
+        current_pos = self.pos()
+
+        # 计算平滑移动的下一个位置
+        next_pos = QPoint(
+            int(lerp(current_pos.x(), target_pos.x(), 0.1)),
+            int(lerp(current_pos.y(), target_pos.y(), 0.1))
+        )
+
+        # 移动窗口到鼠标中心
+        self.move(next_pos)
+        QApplication.processEvents()
 
 
 if __name__ == '__main__':
@@ -38,8 +55,8 @@ if __name__ == '__main__':
     label.show()
     # 创建一个QTimer对象
     timer = QTimer()
-    # 将QTimer对象的timeout信号连接到label的move_to_mouse槽函数
-    timer.timeout.connect(label.move_to_mouse)
+    # 将QTimer对象的timeout信号连接到label的smooth_move槽函数
+    timer.timeout.connect(label.smooth_move)
     # 启动定时器
     timer.start(10)
     # 进入应用程序的主循环，并通过exit函数确保主循环安全结束
